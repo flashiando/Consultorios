1692d963d5d1e8a5b335ad39771e3f52f0dc6bd8
klynjan <javinederlands@gmail.com>
2019-03-09 11:33:43 -0300

    Version 0.1 First Commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // smooth scrolling (completo https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link), revisar
 // smooth scrolling (completo https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link), revisar
-
+//PRUEBA DE REPO DE JAVI
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
