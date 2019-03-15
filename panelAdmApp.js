aa0a1c996a0b885c59c0e072e9791cb07be410a3
Franco Cejudo <francocejudo@gmail.com>
2019-03-15 18:53:47 -0300

    LogIn con LocalStorage listo!

---
+++ panelAdmApp.js
@@ -0,0 +1,25 @@
+
+var administrativo = function (id, Nombre, Apellido, ) {
+    this.id = id;
+    this.nombre = Nombre;
+    this.apellido = Apellido;
+}
+
+
+
+var Admins = [
+    new administrativo('001','Roberto','Html'),
+    new administrativo('002','Tatiana','Zamudio'),
+]
+
+var adminLogueado = undefined; // inicioS() guardara aqui el objeto que representa al Admin logueado.
+
+var inicioS = function () {       
+    adminLogueado = Admins.find(function (element) {
+        return element.id == localStorage.getItem('id');        
+    });
+    document.getElementById("Administrativo").innerHTML = "Panel de control de " + adminLogueado.nombre;
+    alert('Hola')
+}
+
+window.onload = inicioS();
